3102f1c9c48cfaca681cd0cf8433dde8fd1ad7a1
shra14600 <104187381+shra14600@users.noreply.github.com>
2023-05-08 13:29:56 +0530

    commit pages

--- script.js
+++ script.js
@@ -71,42 +71,42 @@ function google() {
 }
 
 // QUIZ Page
-function quizt(frame) {
-  document.getElementById('f1').style='display: none;';
-  document.getElementById('f2').style='display: none;';
-  document.getElementById('f3').style='display: none;';
-  document.getElementById('f4').style='display: none;';
-  document.getElementById('f5').style='display: none;';
-  document.getElementById('f6').style='display: none;';
-  document.getElementById('f7').style='display: none;';
-  document.getElementById('f8').style='display: none;';
-  document.getElementById('f9').style='display: none;';
-  document.getElementById('f10').style='display: none;';
-  document.getElementById('f11').style='display: none;';
-  if(frame == 1) document.getElementById('f1').style = 'display: block';
-  else if(frame == 2) document.getElementById('f2').style = 'display: block';
-  else if(frame == 3) document.getElementById('f3').style = 'display: block';
-  else if(frame == 4) document.getElementById('f4').style = 'display: block';
-  else if(frame == 5) document.getElementById('f5').style = 'display: block';
-  else if(frame == 6) document.getElementById('f6').style = 'display: block';
-  else if(frame == 7) document.getElementById('f7').style = 'display: block';
-  else if(frame == 8) document.getElementById('f8').style = 'display: block';
-  else if(frame == 9) document.getElementById('f9').style = 'display: block';
-  else if(frame == 10) document.getElementById('f10').style = 'display: block';
-  else if(frame == 11) document.getElementById('f11').style = 'display: block'; 
-  else alert('error');
-}
+// function quizt(frame) {
+//   document.getElementById('f1').style='display: none;';
+//   document.getElementById('f2').style='display: none;';
+//   document.getElementById('f3').style='display: none;';
+//   document.getElementById('f4').style='display: none;';
+//   document.getElementById('f5').style='display: none;';
+//   document.getElementById('f6').style='display: none;';
+//   document.getElementById('f7').style='display: none;';
+//   document.getElementById('f8').style='display: none;';
+//   document.getElementById('f9').style='display: none;';
+//   document.getElementById('f10').style='display: none;';
+//   document.getElementById('f11').style='display: none;';
+//   if(frame == 1) document.getElementById('f1').style = 'display: block';
+//   else if(frame == 2) document.getElementById('f2').style = 'display: block';
+//   else if(frame == 3) document.getElementById('f3').style = 'display: block';
+//   else if(frame == 4) document.getElementById('f4').style = 'display: block';
+//   else if(frame == 5) document.getElementById('f5').style = 'display: block';
+//   else if(frame == 6) document.getElementById('f6').style = 'display: block';
+//   else if(frame == 7) document.getElementById('f7').style = 'display: block';
+//   else if(frame == 8) document.getElementById('f8').style = 'display: block';
+//   else if(frame == 9) document.getElementById('f9').style = 'display: block';
+//   else if(frame == 10) document.getElementById('f10').style = 'display: block';
+//   else if(frame == 11) document.getElementById('f11').style = 'display: block'; 
+//   else alert('error');
+// }
 
-function startquiz() {
-  document.getElementById('title').style = 'display: none;'; 
+// function startquiz() {
+//   document.getElementById('title').style = 'display: none;'; 
 
-  document.getElementById('panel').style = 'display: inline-flex;'; 
-  document.getElementById('left').style = 'display: block;'; 
-  document.getElementById('right').style = 'display: block;'; 
-}
-function searchdisplay() {
-  document.getElementById('searchpanel').style.display="block";
-}
+//   document.getElementById('panel').style = 'display: inline-flex;'; 
+//   document.getElementById('left').style = 'display: block;'; 
+//   document.getElementById('right').style = 'display: block;'; 
+// }
+// function searchdisplay() {
+//   document.getElementById('searchpanel').style.display="block";
+// }
 
 function display(n) {
   var img1 = document.getElementById('img1');
