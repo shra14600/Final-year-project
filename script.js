0a7ee35c3aeed4ae0b6fa9a2e69d4b10232fcab6
shra14600 <104187381+shra14600@users.noreply.github.com>
2023-05-10 12:08:27 +0530

    last commit

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
 function scrollAppear() {
-  var introText = document.querySelector('.side-text');
-  var sideImage = document.querySelector('.sideImage');
+  var introText = document.querySelector(".side-text");
+  var sideImage = document.querySelector(".sideImage");
   var introPosition = introText.getBoundingClientRect().top;
   var imagePosition = sideImage.getBoundingClientRect().top;
-  
+
   var screenPosition = window.innerHeight / 1.2;
 
-  if(introPosition < screenPosition) {
-    introText.classList.add('side-text-appear');
+  if (introPosition < screenPosition) {
+    introText.classList.add("side-text-appear");
   }
-  if(imagePosition < screenPosition) {
-    sideImage.classList.add('sideImage-appear');
+  if (imagePosition < screenPosition) {
+    sideImage.classList.add("sideImage-appear");
   }
 }
 
-window.addEventListener('scroll', scrollAppear);
+window.addEventListener("scroll", scrollAppear);
 
 // For switching between navigation menus in mobile mode
 var i = 2;
 function switchTAB() {
-	var x = document.getElementById("list-switch");
-	if(i%2 == 0) {
-		document.getElementById("list-switch").style= "display: grid; height: 50vh; margin-left: 5%;";
-		document.getElementById("search-switch").style= "display: block; margin-left: 5%;";
-	}else {
-		document.getElementById("list-switch").style= "display: none;";
-		document.getElementById("search-switch").style= "display: none;";
-	}
-	i++;
+  var x = document.getElementById("list-switch");
+  if (i % 2 == 0) {
+    document.getElementById("list-switch").style =
+      "display: grid; height: 50vh; margin-left: 5%;";
+    document.getElementById("search-switch").style =
+      "display: block; margin-left: 5%;";
+  } else {
+    document.getElementById("list-switch").style = "display: none;";
+    document.getElementById("search-switch").style = "display: none;";
+  }
+  i++;
 }
 
 // For LOGIN
@@ -55,28 +57,30 @@ function login() {
   a.style.color = "#fff";
   b.style.color = "#000";
 }
-  
+
 // CheckBox Function
-function goFurther(){
+function goFurther() {
   if (document.getElementById("chkAgree").checked == true) {
-    document.getElementById('btnSubmit').style = 'background: linear-gradient(to right, #FA4B37, #DF2771);';
-  }
-  else{
-    document.getElementById('btnSubmit').style = 'background: lightgray;';
+    document.getElementById("btnSubmit").style =
+      "background: linear-gradient(to right, #FA4B37, #DF2771);";
+  } else {
+    document.getElementById("btnSubmit").style = "background: lightgray;";
   }
 }
 
 function google() {
-  	window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
+  window.location.assign(
+    "https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession",
+    "_blank"
+  );
 }
 
 function sideMenu(side) {
-  var menu = document.getElementById('side-menu');
-  if(side==0) {
-    menu.style = 'transform: translateX(0vh); position:fixed;';
-  }
-  else {
-    menu.style = 'transform: translateX(-100%);';
+  var menu = document.getElementById("side-menu");
+  if (side == 0) {
+    menu.style = "transform: translateX(0vh); position:fixed;";
+  } else {
+    menu.style = "transform: translateX(-100%);";
   }
   side++;
 }
